7a5cb3a75c012fbeec7d6ae50c42c9673fc8800b
RomarioLopez <rfls_93@hotmail.com>
2024-02-21 13:38:34 -0600

    11- Destructuracion

---
+++ intro-js/src/11-Destructuracion.js
@@ -0,0 +1,12 @@
+// detructuracion de objetos
+const user = {
+    username: 'Felipe',
+    email: 'felipe@gmail.com',
+    age: 20,
+    ranking: 3
+}
+
+const { username , age} = user;
+
+console.log(username);
+console.log(age);
