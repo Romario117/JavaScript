513a1d188ac69688c15f8f11fd3c0377c361dca6
RomarioLopez <rfls_93@hotmail.com>
2024-02-26 16:08:09 -0600

    Filter methods

---
+++ intro-js/src/14-Filter.js
@@ -0,0 +1,105 @@
+//Filter, Some , Find
+const invoices = [
+    {
+        id:1,
+        nameInvoice: 'Compras',
+        client: {
+            id: 1,
+            name:'fulanito',
+            lastName: 'juarez',
+            age: 20
+        },
+        items:[
+            {
+                product: 'keyboard',
+                price: 90.90,
+                quantity: 10
+            },
+            {
+                product: 'mouse',
+                price: 4.90,
+                quantity: 4
+            },
+            {
+                product: 'monitor',
+                price: 30.88,
+                quantity: 6
+            }, 
+            {
+                product: 'fan',
+                price: 2,
+                quantity: 900
+            }
+        ]
+    },
+    {
+        id:2,
+        nameInvoice: 'Abarrotes',
+        client: {
+            id: 1,
+            name:'fulanito',
+            lastName: 'juarez',
+            age: 20
+        },
+        items:[
+            {
+                product: 'Atun',
+                price: 90.90,
+                quantity: 10
+            },
+            {
+                product: 'Queso',
+                price: 4.90,
+                quantity: 4
+            },
+            {
+                product: 'Sardina',
+                price: 30.88,
+                quantity: 6
+            }, 
+            {
+                product: 'Mayonesa',
+                price: 2,
+                quantity: 900
+            }
+        ]
+    },
+    {
+        id:3,
+        nameInvoice: 'Oficina',
+        client: {
+            id: 3,
+            name:'Juan',
+            lastName: 'Perez',
+            age: 26
+        },
+        items:[
+            {
+                product: 'Hojas',
+                price: 90.90,
+                quantity: 10
+            },
+            {
+                product: 'Clips',
+                price: 4.90,
+                quantity: 4
+            },
+            {
+                product: 'Sillas',
+                price: 30.88,
+                quantity: 6
+            }, 
+            {
+                product: 'Mesa',
+                price: 2,
+                quantity: 900
+            }
+        ]
+    }
+]
+
+const invoicesClone = invoices.find(f => f.client.name === 'fulanito')
+console.log(invoicesClone)
+
+const filtered = invoices.filter(f => f.id >1)
+console.log(filtered)
